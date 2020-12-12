076e45d3e41aa5b9686516a31cc9b634b0f2eccc
amalina-coffee <55571716+amalina-coffee@users.noreply.github.com>
2020-12-12 16:53:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,37 @@
 <?php
   echo 'Hello World';
 ?>
+
+
+<?php
+session_start();
+?>
+	
+	<?php
+	require_once("login_db.php");
+	
+	$sql = "UPDATE Counter SET visits = visits+1";
+	$conn->query($sql);
+	
+	$sql = "SELECT visits FROM Counter";
+	$result = $conn->query($sql);
+	
+	if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $visits = $row["visits"];
+	}
+	
+	} else {
+        echo "no results";
+	}
+
+    
+    ?>
+
+<?php
+$conn->close(); 
+?>
+
+
+<p>You are visitor #: <?php print $visits; ?></p>
+
